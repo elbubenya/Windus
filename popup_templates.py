5b9f7fa065c5eb314fa138609fd0e5f50415a0d9
Jeksonkd <lukiyanovjekson@gmail.com>
2026-08-28 20:00:55 +0300

    popups extended

--- popup_templates.py
+++ popup_templates.py
@@ -1,14 +1,18 @@
-from window import new_window, delete_window, Window, TextWindow
+from pathlib import Path
+
+from window import new_window, delete_window, Window, TextWindow, ExplorerWindow
 from cursor import get_last_id, cursor_select
-from variables import cursor_position
+from variables import cursor_position, home_path
 
 
 desktop_popup = {
     "New window": lambda: new_window(cursor_position["y"], cursor_position["x"], Window),
-    "New text file": lambda: new_window(cursor_position["y"], cursor_position["x"], TextWindow)
+    "New text file": lambda: new_window(cursor_position["y"], cursor_position["x"], TextWindow),
+    "File explorer": lambda: new_window(cursor_position["y"], cursor_position["x"], ExplorerWindow, home_path)
 }
 
 window_popup = {
     "Delete window": lambda: delete_window(get_last_id()),
-    "Select window": lambda: cursor_select(True)
+    "Select window": lambda: cursor_select(True),
+    "Use window": lambda: cursor_select(True, True)
 }
