3ea7b2b992e3224c2ba64cad362b0b2789f1e3d3
Jeksonkd <lukiyanovjekson@gmail.com>
2026-08-28 20:00:32 +0300

    Added esc key handling

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import time
 import keyboard
 
-from variables import windows, popups, cursor_position, directions, key_state
+from variables import windows, popups, cursor_position, directions, key_state, window_used
 from popup import new_popup
-from cursor import cursor_move, cursor_select, cursor_popup, \
+from cursor import cursor_move, cursor_select, cursor_unselect, cursor_popup, \
                    overlapped_window_id, overlapped_popup_button
 from popup_templates import desktop_popup, window_popup
 from render import render
@@ -18,36 +18,38 @@ def track_key(key):
 def handle_enter(_):
     window_popup_open = bool(popups) and popups[0].content is window_popup
 
-    if overlapped_popup_button() is not None:
-        cursor_popup()
-    elif overlapped_window_id() is not None and not window_popup_open:
-        if not key_state["shift"]:
-            if not overlapped_window_id()[1]:
-                cursor_select()
+    if not window_used["value"]:
+        if overlapped_popup_button() is not None:
+            cursor_popup()
+        elif overlapped_window_id() is not None and not window_popup_open:
+            if not key_state["shift"]:
+                if not overlapped_window_id()[1]:
+                    cursor_select()
+                else:
+                    windows.pop(overlapped_window_id()[0])
+                if popups:
+                    popups.pop(0)
             else:
-                windows.pop(overlapped_window_id()[0])
+                new_popup(window_popup, cursor_position["y"], cursor_position["x"] + 1)
+                list(map(lambda window: setattr(window, "selected", 0), windows.values()))
+        else:
             if popups:
                 popups.pop(0)
-        else:
-            new_popup(window_popup, cursor_position["y"], cursor_position["x"] + 1)
+            elif not any(window.selected for window in windows.values()):
+                new_popup(desktop_popup, cursor_position["y"], cursor_position["x"] + 1)
             list(map(lambda window: setattr(window, "selected", 0), windows.values()))
-    else:
-        if popups:
-            popups.pop(0)
-        elif not any(window.selected for window in windows.values()):
-            new_popup(desktop_popup, cursor_position["y"], cursor_position["x"] + 1)
-        list(map(lambda window: setattr(window, "selected", 0), windows.values()))
 
 
 def main():
-    dy, dx = cursor_move(key_state)
+    if not window_used["value"]:
+        dy, dx = cursor_move(key_state)
 
-    if not popups:
-        for window in windows.values():
-            if key_state["shift"]:
-                window.scale_window(key_state)
-            else:
-                window.move_window(dy, dx)
+        if not popups:
+            for window in windows.values():
+                if key_state["shift"]:
+                    window.scale_window(key_state)
+                else:
+                    window.move_window(dy, dx)
 
     render()
     time.sleep(0.075)
@@ -55,6 +57,7 @@ def main():
 
 if __name__ == "__main__":
     keyboard.on_press_key("enter", handle_enter)
+    keyboard.on_press_key("esc", cursor_unselect)
     for tracked_key in list(directions) + ["shift"]:
         track_key(tracked_key)
     os.system("cls" if os.name == "nt" else "clear")
